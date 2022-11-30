2e1f8d5028d83f2c8e672d2340a06c5c6daf9a3f
eliamor6 <107104236+eliamor6@users.noreply.github.com>
2022-11-30 21:23:40 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -1,20 +1,15 @@
 import socket
-import keyboard
-soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cli = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 12345
-soc.connect(('127.0.0.1', port))
-
-def move(direction, soc):
-    soc.send(('MOVE ' + side).encode())
-    print(soc.recv(128).decode())
-
-def status(soc):
-    soc.send('STATUS'.encode())
-    print(soc.recv(128).decode())
-
-keyboard.add_hotkey('up', move, args=('UP', soc))
-keyboard.add_hotkey('down', move, args=('DOWN', soc))
-keyboard.add_hotkey('right', move, args=('RIGHT', soc))
-keyboard.add_hotkey('left', move, args=('LEFT', soc))
-keyboard.add_hotkey('s', args=[soc])
-keyboard.wait('esc')
+cli.connect(('127.0.0.1', port))
+for i in range(10):
+    guess = ""
+    while len(guess) != 4:
+        guess = input("enter 4 digits: ")
+    cli.send(guess.encode())
+    answer = cli.recv(1024).decode()
+    print(answer)
+    if answer == "you got caught:(, maybe next time you'll win" or answer == "Congratulations, you won":
+        cli.close()
+        break
+    
